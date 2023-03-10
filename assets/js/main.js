77ec83821bb0ed0d5ece74b332b043d1660521ff
nicolaazonta <nicolazonta114@gmail.com>
2023-03-10 16:00:11 +0100

    find fizz and buzz numbers and others

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-alert('ocio')
+/* Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz”
+per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo usato un operatore  nei giorni scorsi che possiamo riusare in questi esercizio?
+Consigli del giorno:
+1. Scriviamo sempre prima dei commenti in italiano o inglese per capire cosa vogliamo fare
+2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+3. Facciamo attenzione all'ordine delle condizioni che usiamo */
+
+
+for(let i=1; i<=100; i++){
+    
+    if(Number.isInteger(i / 3)){
+        console.log('FIZZ',i);
+    } else if(Number.isInteger(i / 5)){
+        console.log('BUZZ',i);                
+    }else{
+        console.log(i);
+    }
+    
+    //console.log(i);
+}
+
+
+
+
+
+
+
+
